04f6a2efc37677d41796ce0beca08b7b2985d630
kaiasdad01 <mjbdwyer@gmail.com>
2025-11-02 19:05:20 -0700

    fixed hanging dbt dag

--- README.md
+++ README.md
@@ -47,3 +47,11 @@ For p1 - I'll continue to add data sources, implement additional predictive mode
 ### Questions + Thoughts
 **Couldn't you have used cron + other simpler services** - Yes! But, I like to use these tools & services to continue learning - especially for the products that I don't get to use in my day job (i.e., the non-AWS stuff). 
 
+### Things to fix & improve
+- [ ] Right now, loading with a replace functionality. Update this to just look for new records and append. 
+
+- [ ] Injuries keeps breaking. Removed for now, will investigate and re-add. Working hypothesis is that the Seasons param is the root cause. 
+
+- [ ] Rename / reorg BQ datasets 
+
+- [ ] Mage looks cool?
--- airflow/dags/nfl_dbt_transform.py
+++ airflow/dags/nfl_dbt_transform.py
@@ -1,12 +1,39 @@
-from airflow import DAG
+import sys
 from datetime import datetime, timedelta
+
+from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.sensors.external_task import ExternalTaskSensor
-import sys
 
 sys.path.insert(0, '/opt/airflow/nfl_v3/airflow')
 
 from utils.nfl_tasks import run_dbt
+
+
+def get_latest_external_dag_run_date(logical_date, **kwargs):
+    """Returns the execution_date of the most recent successful load DAG run"""
+    from airflow.models import DagRun
+    from airflow.utils.db import create_session
+
+    external_dag_id = 'nfl_data_load'
+
+    with create_session() as session:
+        latest_run = (
+            session.query(DagRun)
+            .filter(
+                DagRun.dag_id == external_dag_id,
+                DagRun.state == 'success'
+            )
+            .order_by(DagRun.execution_date.desc())
+            .first()
+        )
+
+        if latest_run:
+            print(f"Found latest successful {external_dag_id} run at {latest_run.execution_date}")
+            return latest_run.execution_date
+
+    print(f"No successful {external_dag_id} run found, using logical_date {logical_date}")
+    return logical_date
 
 default_args = {
     'owner': 'airflow',
@@ -22,7 +49,7 @@ dag = DAG(
     'nfl_dbt_transform',
     default_args=default_args,
     description='Transform raw NFL data in BigQuery using dbt after load DAG completes',
-    schedule_interval='0 7 * * 5',  
+    schedule_interval='0 7 * * 5',
     catchup=False,
     tags=['nfl', 'dbt', 'transform'],
 )
@@ -31,10 +58,14 @@ dag = DAG(
 wait_for_load = ExternalTaskSensor(
     task_id='wait_for_load',
     external_dag_id='nfl_data_load',
+    external_task_id=None,
     allowed_states=['success'],
+    failed_states=None,
+    check_existence=True,
     mode='reschedule',
-    poke_interval=60,  # Check every minute
-    timeout=6 * 60 * 60,  # 6 hour timeout
+    poke_interval=60,
+    timeout=6 * 60 * 60,
+    execution_date_fn=get_latest_external_dag_run_date,
     dag=dag,
 )
 
